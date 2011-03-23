ab6840ec7db134269c7af9a04565cdd4acce2eb6
rjeitani <rjeitani@gmail.com>
2011-03-23 06:19:30 +0000

    Added types.h which contains struct for functions

---
+++ src/types.h
@@ -0,0 +1,23 @@
+#ifndef _TYPES_H
+#define _TYPES_H
+
+
+typedef struct
+{
+	
+	int fid;
+	int numCubes;
+	int numVars;
+	char cubes[15][26]; // stores 15 cubes of 26 characters each
+	char kernels[15][26];
+	char cokernels[15][26];
+
+} func;
+
+
+#ifndef NULL
+#define NULL    ((void*)0)
+#endif
+
+#endif
+
