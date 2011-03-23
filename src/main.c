85201199b20cec9bc53b8c1fa8bf6a9fc1a9fb77
rjeitani@gmail.com <rjeitani@gmail.com@61d27062-5b7b-f1be-ac95-5a83b0370e73>
2011-03-23 04:52:07 +0000

    Now displays number of variables and functions with checking

--- src/main.c
+++ src/main.c
@@ -10,8 +10,22 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <xlocale.h>
 
 #define ARG_NUM 2 // number of arguments to expect
+
+int VAR_FLAG = 0; // Set to 1 when number of variable has been read
+int FUNC_FLAG = 0; // Set to 1 when number of functions has been read
+int BODY_FLAG = 0; // Set to 1 when body of input file is entered
+
+
+void readFunction()
+{
+	
+	// parse each function
+	
+}
+
 
 
 int main (int argc, char* argv[])
@@ -20,7 +34,7 @@ int main (int argc, char* argv[])
     // Check for number of arguments
     if (argc < ARG_NUM)
     {
-        printf("At least %d arguments are required.\n", ARG_NUM);
+        printf("At least %d argument(s) required.\n", (ARG_NUM - 1));
         exit(1);
     }
     
@@ -36,7 +50,7 @@ int main (int argc, char* argv[])
     }
     
     size_of_file = file_stat.st_size;
-    printf("Size of file: %d\n", size_of_file);
+    printf("Size of file: %d\n", (int) size_of_file);
     
     fp = fopen(argv[1], "r"); // open file for reading
     if (fp == NULL) 
@@ -49,10 +63,86 @@ int main (int argc, char* argv[])
 
     int count;
     count = 1;
+	
+	int j = 0;
+	
+	int numberOfFunctions;
+	int numberOfVariables;
     
     while (fgets(line, 80, fp) != NULL)
     {
-        printf("Just read line number: %d\n", count);
+		// read in the file one line at a time
+		// 1st line is number of local functions
+		// 2nd line is number of variables
+		
+		
+		if (VAR_FLAG == 1 && FUNC_FLAG == 1 && BODY_FLAG == 1)
+		{
+			// processing the functions
+			
+			
+			printf("Line Contents: %s", line);
+			
+			if (j == numberOfFunctions)
+			{
+				printf("Error: You specified %d functions, just read number %d.\n", numberOfFunctions, j+1);
+			}
+			
+			//printf("j=%d\n", j);
+			j++;
+			
+			
+			
+		}
+		
+				
+		if (VAR_FLAG == 1 && FUNC_FLAG == 1 && BODY_FLAG == 0) {
+			if (line[0] == '\n') {
+				BODY_FLAG = 1;
+			}
+			else {
+				printf("Error: You must have a blank line before the body.\n");
+				exit(1);
+			}
+
+		}
+		
+		
+		if (line[0] == '.' && FUNC_FLAG == 1 && VAR_FLAG == 0) {
+			
+			if (line[2] == '\n') {
+				numberOfVariables = ((int)line[1] - 48);
+			}
+			else {
+				numberOfVariables = (((int)line[1] - 48) * 10) + ((int)line[2] - 48);
+			}
+			
+			VAR_FLAG = 1;
+			
+			printf("Number of Variables: %d\n", numberOfVariables);
+			if (numberOfVariables > 26) {
+				printf("Too many variables. Maximum is 26.\n");
+				exit(1);
+			}
+		}
+	
+		if (line[0] == '.' && FUNC_FLAG == 0) {
+				if (line[2] == '\n') {
+					numberOfFunctions = ((int)line[1] - 48);
+									}
+			else {
+				numberOfFunctions = (((int)line[1] - 48) * 10) + ((int)line[2] - 48);
+			}
+			FUNC_FLAG = 1; 
+
+			printf("Number of Functions: %d\n", numberOfFunctions);
+			if (numberOfFunctions > 20) {
+				printf("Too many input functions. Maximum is 20.\n");
+				exit(1);
+			}
+		
+		}
+		
         count++;
     }
        
